dbf3d1e7585ce30934bcc0bd6be58e6afcc9f1cf
marvinlaisney04 <laisneymarvin@gmail.com>
2022-04-13 16:30:09 +0200

    first commit

---
+++ src/main/java/com/hopital/chambrems/ChambremsApplication.java
@@ -0,0 +1,13 @@
+package com.hopital.chambrems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChambremsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChambremsApplication.class, args);
+	}
+
+}
